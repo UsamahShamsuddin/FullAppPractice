b0f4e72bb274bf93f9685e66ab744c3a182058fa
usamah <usamdsb92@gmail.com>
2023-03-12 18:29:19 +0800

    Add the Web API Project
    Adds a blank Web API project and tweaks it

---
+++ FAPDataManager/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace FAPDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
---
+++ FAPDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FAPDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
