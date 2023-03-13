12aa3ee1cc22f9aedf1f881dd623f4827ea21196
usamah <usamdsb92@gmail.com>
2023-03-13 21:33:29 +0800

    Add DB Tables
    Adds the tables for Bug Tracker system.

---
+++ FAPData/dbo/Tables/Bugs.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Bugs]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProjectId] INT NOT NULL, 
+    [TItle] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [BugStatus] INT NOT NULL, 
+    [Priority] INT NOT NULL, 
+    [CreatedById] NVARCHAR(128) NOT NULL,
+    [AssignedToId] NVARCHAR(128) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(),   
+    [ClosedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [Comments] NVARCHAR(MAX) NOT NULL,
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
---
+++ FAPData/dbo/Tables/Projects.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Projects]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProjectName] NVARCHAR(50) NOT NULL, 
+    [ProjectStatus] INT NOT NULL, 
+    [CreatedById] NVARCHAR(128) NOT NULL,
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [ClosedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
